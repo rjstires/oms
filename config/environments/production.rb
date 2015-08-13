03cbb67a46cb6b5bcbcab292c525652cdea8e6df
Jarvis Dresden <jarvis.dresden@gmail.com>
2015-08-13 08:09:48 +0000

    ActionMailer default host.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = false
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
 
 
@@ -92,4 +92,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { :host => "oms.boostingedge.com" }
 end
