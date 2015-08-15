4a759efef48b74ecdb5123c7d2aa680bc6b7f0c5
Jarvis Dresden <jarvis.dresden@gmail.com>
2015-08-15 21:00:28 +0000

    Temporary fix for date display on order_line#index

--- app/helpers/order_lines_helper.rb
+++ app/helpers/order_lines_helper.rb
@@ -1,33 +1,4 @@
 module OrderLinesHelper
-  def date_column_header
-    case params[:status]
-    when 4
-      title = 'Completed On'
-    when 1
-      title = 'Scheduled For'
-    when 2
-      title = 'Created On'
-    when 3
-      title = 'Created On'
-    end
-
-    "<th>#{title}</th>".html_safe unless title.nil?
-  end
-
-  def date_value(order)
-    case params[:status]
-    when 4
-      date = order.completed_at
-    when 1
-      date = order.scheduled_at
-    when 2
-      date = order.created_at
-    when 3
-      date = order.created_at
-    end
-
-    "<td>#{date.to_s(:scheduled_for)}</td>".html_safe unless date.nil?
-  end
 
   def status_link_text(o)
     "#{o.name.titleize} <span class=\"badge\"></span>".html_safe
