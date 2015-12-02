dec088d9edf6e5d3a3808f0bc6d9871a47155e9c
Robert John <rjstires@gmail.com>
2015-12-02 06:50:13 +0000

    Add dashboard cards

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,9 +1,13 @@
 class Admin::DashboardController < AdminController
   def index
+    @incomplete_orders = OrderLine.where_order_paid.where_not_completed
+    @unsettled_funds = @incomplete_orders.sum(:sale).to_i
+
     @orders_dispatched = OrderLine.index_join.scheduled.upcoming_event.scheduled_at_asc
     @orders_ready_to_dispatch = OrderLine.index_join.ready_to_schedule.created_at_asc
     @orders_past_due = OrderLine.index_join.where_not_completed.past_event.scheduled_at_asc
     @orders_lead = OrderLine.index_join.lead.created_at_asc
+
     store_location
   end
 end
