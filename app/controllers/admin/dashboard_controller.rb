f1a8730a3379a5585f6d646746d781943ffc9108
Robert John <rjstires@gmail.com>
2015-12-02 07:19:03 +0000

    Finalize and format dahsboard cards

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,8 @@
 class Admin::DashboardController < AdminController
   def index
     @incomplete_orders = OrderLine.where_order_paid.where_not_completed
-    @unsettled_funds = @incomplete_orders.sum(:sale).to_i
+    @unsettled_sale = @incomplete_orders.sum(:sale).to_i
+    @unsettled_commission = @incomplete_orders.sum(:site_fee).to_i
 
     @orders_dispatched = OrderLine.index_join.scheduled.upcoming_event.scheduled_at_asc
     @orders_ready_to_dispatch = OrderLine.index_join.ready_to_schedule.created_at_asc
