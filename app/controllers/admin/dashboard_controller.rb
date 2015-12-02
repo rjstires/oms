0325e9fa6413829d3b2b606cb688e4b6eea6d1cc
Robert John <rjstires@gmail.com>
2015-12-02 23:37:11 +0000

    Ammend last commit

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -5,9 +5,9 @@ class Admin::DashboardController < AdminController
     @unsettled_commission = @incomplete_orders.sum(:site_fee).to_i
 
     @orders_dispatched = OrderLine.index_join.scheduled.upcoming_event.scheduled_at_asc
-    @orders_ready_to_dispatch = OrderLine.index_join.ready_to_schedule.created_at_asc
+    @orders_ready_to_dispatch = OrderLine.index_join.ready_to_schedule.order_asc
     @orders_past_due = OrderLine.index_join.where_not_completed.past_event.scheduled_at_asc
-    @orders_lead = OrderLine.index_join.lead.order_asc
+    @orders_lead = OrderLine.index_join.lead
 
     store_location
   end
