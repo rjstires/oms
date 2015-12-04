b245c5c21939dccd83b6f85c383992b943264b1c
Robert John <rjstires@gmail.com>
2015-12-04 08:37:24 +0000

    Bad, this is bad.

--- db/migrate/20151203200246_create_event_slots.rb
+++ db/migrate/20151203200246_create_event_slots.rb
@@ -1,12 +1,13 @@
 class CreateEventSlots < ActiveRecord::Migration
-  def change
-    create_table :event_slots do |t|
-      t.references :event, index: true, foreign_key: true
-      t.string :title
-      t.text :description
-      t.references :character, index: true, foreign_key: true
+	def change
+	drop_table :event_slots
+		create_table :event_slots do |t|
+			t.references :event, index: true, foreign_key: true
+			t.string :title
+			t.text :description
+			t.references :character, index: true, foreign_key: true
 
-      t.timestamps null: false
-    end
-  end
+			t.timestamps null: false
+		end
+	end
 end
