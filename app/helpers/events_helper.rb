23d8b4bcbbf7b97845946ebc5d17c4b53c838968
Robert John <rjstires@gmail.com>
2015-12-04 07:34:42 +0000

    New layout and styling for admin/events

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,11 @@
 module EventsHelper
+	
+	def event_row_class(event)
+		event.team.faction.name.parameterize
+	end
+
+	def event_slot_row_class(slot)
+		slot.character.present? ? "not-vacant" : "vacant"
+	end
+
 end
