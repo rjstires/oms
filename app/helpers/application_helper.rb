fdd924faaa500210e90d27c52f8f9a972f22311a
Robert John <rjstires@gmail.com>
2015-12-05 03:57:31 +0000

    Reorganize site wide meny. Create sub_nav_link helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,14 @@ module ApplicationHelper
 	def status_link_text(o)
 		"#{o.name.titleize} <span class=\"badge\"></span>".html_safe
 	end
+
+  def sub_nav_link(title, path, icon=nil)
+    icon = "<i class=\"fa fa-#{icon}\"></i>" if icon.present?
+    content_tag :li, class: 'sub-nav' do
+      link_to path do
+        "#{icon} #{title}".html_safe
+      end
+    end
+  end
+
 end
