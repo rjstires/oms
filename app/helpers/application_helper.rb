30cc7b4452edbe1ab830ab4fda3cdbb00b183ba3
Robert John <rjstires@gmail.com>
2015-12-05 10:11:52 +0000

    Refactor nav_link helper function to use options

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,14 +4,17 @@ module ApplicationHelper
 		"#{o.name.titleize} <span class=\"badge\"></span>".html_safe
 	end
 
-  def nav_link(title, path, klass=nil, icon=nil)
-    icon = "<i class=\"fa fa-#{icon}\"></i>" if icon.present?
+  def nav_link(title, path, options = {})
+    klass = options.fetch(:klass, '')
+    icon = options.fetch(:icon, nil)
+
+    icon_string = "<i class=\"fa fa-#{icon}\"></i>".html_safe if icon.present?
+
     content_tag :li, class: klass do
       link_to path do
-        "#{icon}<span>#{title}</span>".html_safe
+        "#{icon_string}<span>#{title}</span>".html_safe
       end
     end
   end
-
 
 end
