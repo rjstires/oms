7e45a52bfef01ea5989c742f1f717dadf1395b3d
Robert John <rjstires@gmail.com>
2015-12-05 05:36:24 +0000

    Refactor order_lines table to partial. Add upcoming orders to team#show

--- app/controllers/order_lines_controller.rb
+++ app/controllers/order_lines_controller.rb
@@ -2,25 +2,22 @@ class OrderLinesController < ApplicationController
   load_and_authorize_resource :team
   load_and_authorize_resource :order_line, :through => :team
 
-  before_filter :set_default_category, :only => [:index]
-
   # GET /order_lines
   # GET /order_lines.json
   def index
     @dispatched_orders = @team
     .order_lines
     .index_join
-    .filter( params.slice(:status, :category, :difficulty) )
-    .scheduled
+    .where_scheduled
+    .where_not_completed
     .accessible_by(current_ability)
 
     @completed_orders = @team
     .order_lines
     .index_join
-    .filter( params.slice(:status, :category, :difficulty) )
-    .where_completed  
+    .where_completed
     .accessible_by(current_ability)
-    
+
     store_location
   end
 
@@ -29,11 +26,4 @@ class OrderLinesController < ApplicationController
   end
 
   private
-
-  def set_default_category
-    if params[:category].blank?
-      params[:category] = Category.by_name('raiding').id
-      redirect_to url_for(params)
-    end
-  end
 end
