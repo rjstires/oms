a0737767cbc67204b4fff2a9e4122a2527cc89d7
Robert John <rjstires@gmail.com>
2015-12-07 03:16:50 +0000

    Refactor tile displays. Adjust dashbards.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,48 @@ module ApplicationHelper
       end
     end
   end
+
+  def tile_color(color)
+    accepted_colors = [
+      'green',
+      'red',
+      'blue',
+      'orange',
+      'prusia',
+      'lemon',
+      'purple',
+      'concrete'
+    ]
+
+    accepted_colors.include?(color) ? color : 'concrete'
+
+  end
+
+  def render_clean_tile(options={})
+    render partial: "shared/clean_tile", locals: {
+      title: options.fetch(:title),
+      number: options.fetch(:number),
+      icon: options.fetch(:icon),
+      path: options.fetch(:path, nil),
+      color: options.fetch(:color, "concrete")
+    }
+  end
+
+  def render_clean_tile_details(path=nil)
+    unless path.nil?
+      link_to path, class: 'details' do
+        "&nbsp;<span>DETAILS <i class=\"fa fa-arrow-circle-right pull-right\"></i></span>".html_safe
+      end
+    end
+  end
+
+  def render_header_tile(options={})
+
+    render partial: "shared/header_tile", locals: {
+      display_header: options.fetch(:header),
+      display_text: options.fetch(:text, '&nbsp;'.html_safe),
+      display_number: options.fetch(:number, nil),
+      color: options.fetch(:color, "concrete")
+    }
+  end
 end
