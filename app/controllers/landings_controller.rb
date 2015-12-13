c6e511824f1635defcd584eb4e5d3ce49234b752
Robert John <rjstires@gmail.com>
2015-12-13 05:56:20 +0000

    Redirect user to first team dashboard on login

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -2,9 +2,10 @@ class LandingsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-      path = 'dashboard'
-      path = 'admin_root' if current_user.admin?
+    path = dashboard_path.to_s
+    path = dashboard_team_path(current_user.teams_id_list.first).to_s if current_user.teams_id_list.any?
+    path = admin_root_path.to_s if current_user.admin?
 
-      redirect_to path.to_sym
+    redirect_to path
   end
 end
