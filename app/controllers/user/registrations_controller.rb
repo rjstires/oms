01f5d3c4be70993772ebfbb3a31304037c3035cf
Robert John <rjstires@gmail.com>
2015-12-19 11:36:38 +0000

    Add admin mailer on new user registration

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -1,22 +1,29 @@
 class User::RegistrationsController < Devise::RegistrationsController
   before_action :update_sanitized_params
 
+  def create
+    super
+    if @user.persisted?
+      AdminMailer.new_registration(@user).deliver
+    end
+  end
+
   protected
 
   def update_sanitized_params
-	devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(
-		:name,
-		:email,
-		:battle_tag, 
-		:skype,
-		:password,
-		:password_confirmation
-	)}
-  end
+   devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(
+    :name,
+    :email,
+    :battle_tag,
+    :skype,
+    :password,
+    :password_confirmation
+    )}
+ end
 
-  private
+ private
 
-	def after_inactive_sign_up_path_for(resource)
-		new_user_session_path
-	end
+ def after_inactive_sign_up_path_for(resource)
+  new_user_session_path
+end
 end
