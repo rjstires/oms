c2b9ff97f8b48e424d56dbfc1f6ccf1fe334c5b7
Robert John <rjstires@gmail.com>
2015-12-19 17:29:07 +0000

    Update admin new user mailer to use Mandril

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,13 +1,14 @@
-class AdminMailer < ApplicationMailer
-  default from: 'Administrator <noreply@boostingedge.com>'
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.admin_mailer.new_registration.subject
-  #
+class AdminMailer < BaseMandrillMailer
+
   def new_registration(user)
-    @user = user
-    mail to: User.admin.pluck(:email)
-    mail subject: "A new user has registered with Boosting Edge, LLC"
+    subject = "[Boosting Edge, LLC] New user notification."
+    merge_vars = {
+      "USER_NAME" => user.email,
+      "USER_EMAIL" => user.name,
+      "PATH" => admim_user_path(user),
+    }
+
+    body = mandrill_template("new_user_invite", merge_vars)
+    send_mail(invite.email, subject, body)
   end
 end
